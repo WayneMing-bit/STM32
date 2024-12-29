0c5aa9d0e47c3e3f0bc9fdd31072575b940546ed
Wayne Ming <badmanweimin@gmail.com>
2024-12-29 13:42:38 +0800

    LED闪烁

---
+++ 3-1 LED闪烁/User/main.c
@@ -0,0 +1,28 @@
+#include "stm32f10x.h"                  // Device header
+
+int main(void) {
+//    // ¼Ä´æÆ÷²Ù×÷
+//		RCC->APB2ENR = 0x00000010;
+//		GPIOC->CRH = 0x00300000;
+//		GPIOC->ODR = 0x00002000;
+		
+		/**
+			STM32¿âº¯Êý²Ù×÷ 
+		*/
+		RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOC, ENABLE);
+	
+		// GPIO¿âº¯Êý²Ù×÷
+		GPIO_InitTypeDef GPIO_InitStruct;
+		GPIO_InitStruct.GPIO_Mode = GPIO_Mode_Out_PP;
+		GPIO_InitStruct.GPIO_Pin = GPIO_Pin_13;
+		GPIO_InitStruct.GPIO_Speed = GPIO_Speed_50MHz;
+		GPIO_Init(GPIOC, &GPIO_InitStruct);
+		
+		// GPIO¿âº¯ÊýµãµÆ
+		GPIO_SetBits(GPIOC, GPIO_Pin_13);
+//		GPIO_ResetBits(GPIOC, GPIO_Pin_13);
+	
+		while (1) {
+			
+		}
+}
